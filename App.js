4a0d04d9be9771f5025e1bcb7658f547ae2ef918
arunmudev <45461966+arunmudev@users.noreply.github.com>
2020-12-20 17:43:12 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,12 +3,6 @@ import { Provider } from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import store from './app/redux/store';
-// import Welcome from './src/Welcome';
-// import Login from './src/Login';
-// import Signup from './src/Signup';
-// import ProductsDashboard from './src/ProductsDashboard';
-// import Checkout from './src/Checkout';
-// import Receipt from './src/Receipt';
 import Route from './app/routes/Route';
 
 export default  App = () => {
